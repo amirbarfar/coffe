61b0c3ba2cbe21714f96ace87602713d8abedb68
amirbarfar <amirbarfar43@gmail.com>
2025-03-11 04:13:05 +0330

    fix navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image"
 import Link from "next/link";
-import { SetStateAction, useState } from "react"
+import { useState } from "react"
 
 export default function Header() {
 
