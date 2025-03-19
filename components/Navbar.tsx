c5acf5b036d8f30d1d576bdda3dd99d7c353e280
amirbarfar <amirbarfar43@gmail.com>
2025-03-20 02:18:22 +0330

    fix header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -42,7 +42,6 @@ export default function Navbar() {
                     numSquares={30}
                     maxOpacity={0.1}
                     duration={3}
-                    repeatDelay={1}
                     className={cn(
                         "[mask-image:radial-gradient(1000px_circle_at_center,white,transparent)]",
                         "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
