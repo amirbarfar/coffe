d7137176fc59e22aaac4027a485c2901d469ce57
amirbarfar <amirbarfar43@gmail.com>
2025-06-12 21:50:39 +0330

    fix bug and add shop to home

--- app/pages/Shop.tsx
+++ app/pages/Shop.tsx
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { dataShop } from '@/consts/consts';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -9,16 +8,41 @@ import 'swiper/css/navigation';
 
 // import required modules
 import { Autoplay } from 'swiper/modules';
+import clsx from 'clsx';
 
 export default function Shop() {
+
+    const dataShop = [
+        {
+            tools: [
+                { id: 1, image: './images/shopImages/1.png', name: "اسپرسو ماشین", price: '2500000', discount: '20' },
+                { id: 2, image: './images/shopImages/1.png', name: "آسیاب قهوه", price: '100000', discount: '12' },
+                { id: 3, image: './images/shopImages/1.png', name: "تمپر", price: '32000', discount: '15' },
+                { id: 4, image: './images/shopImages/1.png', name: "موکاپات", price: '54200', discount: '10' },
+                { id: 5, image: './images/shopImages/1.png', name: "موکاپات", price: '54200', discount: '10' },
+                { id: 6, image: './images/shopImages/1.png', name: "موکاپات", price: '54200', discount: '10' },
+
+            ],
+            consumables: [
+                { id: 1, image: './images/shopImages/2.png', name: "دانه قهوه (روست‌های مختلف: اسپرسو، ترکیبی، تک‌خاستگاه و...)", price: '250000', discount: '12' },
+                { id: 2, image: './images/shopImages/2.png', name: "شیر (حیوانی یا گیاهی مثل بادام، جو، نارگیل)", price: '35000', discount: '30' },
+                { id: 3, image: './images/shopImages/2.png', name: "شربت‌های طعم‌دهنده (وانیل، کارامل، فندق...)", price: '54000', discount: '10' },
+                { id: 4, image: './images/shopImages/2.png', name: "پودر شکلات، دارچین، هل", price: '98200', discount: '20' },
+                { id: 5, image: './images/shopImages/2.png', name: "پودر شکلات، دارچین، هل", price: '98200', discount: '20' },
+                { id: 6, image: './images/shopImages/2.png', name: "پودر شکلات، دارچین، هل", price: '98200', discount: '20' },
+
+            ]
+        }
+    ]
 
     const progressCircle = useRef<HTMLDivElement>(null);
     const progressContent = useRef<HTMLSpanElement>(null);
 
-    const onAutoplayTimeLeft = (time: number, progress: number) => {
+    const onAutoplayTimeLeft = (s: any, time: number, progress: number) => {
         if (progressCircle.current) {
             progressCircle.current.style.setProperty('--progress', `${1 - progress}`);
         }
+        console.log(s)
         if (progressContent.current) {
             progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
         }
